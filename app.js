090dc942c5e78fa7234d9c9a00420c5e849f9b01
Thanapuet Kaewmanee <kthanapuet@gmail.com>
2022-04-21 15:02:56 +0700

    Re new pdf project

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.raw());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/PostNotification', (req, res) => {
-    console.log("Source IP: " + requestIp.getClientIp(req));
-    console.log(req.body.pernr);
-    res.send(JSON.stringify(req.body));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Default IP control with a whitelist
-app.get('/start', (req, res) => {
-    res.send("Hello Nodejs App.");
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
