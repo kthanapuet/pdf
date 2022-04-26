327aff3429d783f8b738908fd8f3a32d0838d0bf
Thanapuet Kaewmanee <kthanapuet@gmail.com>
2022-04-26 16:25:54 +0700

    add node controll pdf

---
+++ controller/pdf.js
@@ -0,0 +1,26 @@
+const { degrees, PDFDocument, rgb, StandardFonts } = require('pdf-lib')
+
+module.exports = {
+    modifyPdf: async () => {
+        const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+        const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+        const pdfDoc = await PDFDocument.load(existingPdfBytes)
+        const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+        const pages = pdfDoc.getPages()
+        const firstPage = pages[0]
+        const { width, height } = firstPage.getSize()
+        firstPage.drawText('This text was added with JavaScript!', {
+            x: 5,
+            y: height / 2 + 300,
+            size: 50,
+            font: helveticaFont,
+            color: rgb(0.95, 0.1, 0.1),
+            rotate: degrees(-45),
+        })
+
+        const pdfBytes = await pdfDoc.save()
+        return pdfBytes
+    }
+}
--- routes/pdf.js
+++ routes/pdf.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
+const pdf = require('../controller/pdf')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("PDF Library");
 });
+
+router.get('/waterMask', (req, res) => {
+  res.download(pdf.modifyPdf())
+})
 
 module.exports = router;
